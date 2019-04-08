48e5d9dd2f33fe1875ea607806ef9ee9fb78dbda
Timo P. Becker <3785547+timopb@users.noreply.github.com>
2019-04-08 14:57:23 +0200

    added helper scripts

---
+++ debug.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+export MQTT_SERVER=your.mqtt.server
+export MQTT_USERNAME=YourAccountGoesHere
+export MQTT_PASSWORD=Password123abc!
+export MQTT_TOPIC=some/topic
+
+python ./main.py
---
+++ push.sh
@@ -0,0 +1,2 @@
+#!/bin/sh
+docker push <your docker hub account>/rpi-cputemp-mqtt
